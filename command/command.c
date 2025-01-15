3b27d89c2f2055baa5565ba6be88b805e41b12af
R Peavey <rpeavey@c2r1s9.42singapore.sg>
2025-01-15 19:14:20 +0800

    added more scaffolding

---
+++ command/command.c
@@ -0,0 +1,26 @@
+#include "command.h"
+
+t_cmd	init_command(char *input)
+{
+	//TODO
+}
+
+void	destroy_command(t_cmd c)
+{
+	//TODO
+}
+
+int	tokenize_command(t_cmd c)
+{
+	//TODO
+}
+
+int	parse_command(t_state s, t_cmd c)
+{
+	//TODO
+}
+
+void	exec_command(t_cmd c)
+{
+	//TODO, interface with EXECUTE?
+}
