1202aedab4d5c36dbfe270e0dbda569e73445797
russpv <russellpeavey@gmail.com>
2025-01-17 15:31:11 +0800

    added more notes to parse.c

--- parse/parse.c
+++ parse/parse.c
@@ -52,10 +52,66 @@
  * inside escape sequence
  * operator parsing
  */
-
-
 // TODO use a stack to track open/close delimiters
+// track state with flags in_single_quotes, in_double_quotes, escaping
+// treat space as delimiter only if all these are false
+// toggle quotes if escaping is false
+
+/* On separators
+ *
+ * "A cornerstone of UNIX wisdom is that “plaintext is the 
+ * universal interface.” Hence, it is not strongly typed; it’s stringly typed."
+ * - a grumpy programmer
+ */
+// Notes: These bash separators must be lexed properly
+// line-separator \n (treated as separate commands) vs continuation '\'
+// but continuation char not needed for pipes
+// continuation expected for heredocs
+// continuation is equivalent of ';'
+// 
+// Be careful of spaces var="name with space" will expand to a list, not a string
+// bash doesn't allow this to expand to two arguments: var='a "b c"', just 1 or 3
 //
+// Colon 
+// Could be in a dir or filename, but for simplicity will treat as a char, or 
+// the PATH delimiter
+//
+/* The State Machine
+ * General states:
+ * START: Beginning of input or start of a new token.
+ * IN_WORD: Collecting characters in a word token.
+ * IN_OPERATOR: Collecting characters in an operator token.
+ * IN_QUOTE: Processing quoted text.
+ * IN_SUBSTITUTION: Processing parameter, command, or arithmetic substitution.
+ * IN_COMMENT: Discarding characters in a comment.
+ * IN_HEREDOC: 
+ * END: End of input.
+ *
+ * Use token buffer and token list 
+ *
+ * Traverse input ONCE
+ *
+ * Handle these rules
+ * Hash - Ignore everything till newline
+ * Whitespace - discard unquoted space and finalize current token
+ * Start char - start a new token
+ * Quotes - track ' " or \ and collect until closing symbol found
+ * Operators - look ahead one char for double char ops
+ * Null term - finalize current token
+ * (distinguish reserved words)
+ * (remove escape chars)
+ *
+ * Tokens
+ * TOKEN: WORD, ASSIGNMENT_WORD, NAME, (reserved word)
+ * OPERATOR: (below)
+ * NEWLINE
+ * IO_NUMBER e.g. &2>1
+ *
+ * AND_IF
+ * OR_IF
+ * DLESS
+ * DGREAT
+ */ 
 
 typedef enum {
 	TOK_CMD,
