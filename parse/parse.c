89ef6f0e588085063a959905636016ae169c0ea1
R Peavey <rpeavey@c2r1s11.42singapore.sg>
2025-01-17 18:48:44 +0800

    started a State pattern lexer

--- parse/parse.c
+++ parse/parse.c
@@ -113,22 +113,124 @@
  * DGREAT
  */ 
 
+
+#define TK_EOF "\0"
+#define TK_STAT "$?"
+#define TK_NEWL	"\n"
+#define TK_ESC "\\"
+
+#define OP_EQUALS "="
+#define OP_HASH "#" // skip everything till newline
+#define OP_ANDIF "&&"
+#define OP_FD_REF "&" // redirection
+#define OP_ORIF "||" 
+#define OP_REDOUT ">"
+#define OP_REDIN "<"
+#define OP_DGREAT ">>"
+#define OP_DLESS "<<"
+#define OP_PIPE "|"
+#define OP_ENV "$"  // this plus underscore or alphanumeric, an env_var
+#define OP_STAR "*"
+#define OP_SQUOTE "\'"
+#define OP_DQUOTE "\"" // backslash retained if followed by [$`"\\n], " requires \ inside ""
+#define OP_DLESSDASH "<<-" // heredoc with opt tab trimming (not implemented)
+#define OP_CLOBBER ">|" // force overwrite (not implemented)						   
+
+/* Reserved words */
+#define WD_IF "if"
+#define WD_THEN "then"
+#define WD_ELSE "else"
+#define WD_FI "fi"
+#define WD_DO "do"
+#define WD_DONE "done"
+#define WD_CASE "case"
+#define WD_ESAC "esac"
+#define WD_WHIL	"while"
+#define WD_UNTL	"until"
+#define WD_FOR	"for"
+#define WD_IN	"in"
+
 typedef enum {
-	TOK_CMD,
-	TOK_ARG,
-	TOK_OP,
-	TOK_QUOTE,
-	TOK_ESCAPE,
-	TOK_UNK
-} e_tok_type
+    TOK_WORD, // command names, built-ins
+    TOK_ASSIGNMENT_WORD,
+    TOK_NAME,
+    TOK_NEWLINE,
+    TOK_IO_NUMBER, // REDOUT/REDIN plus digits
+    TOK_REDIRECT_IN,
+    TOK_REDIRECT_OUT,
+    TOK_REDIRECT_APPEND,
+    TOK_HEREDOC,
+    TOK_PIPE,
+    TOK_SINGLE_QUOTE,
+    TOK_DOUBLE_QUOTE,
+    TOK_ENV_VAR,
+    TOK_EXIT_STATUS,
+    TOK_IF,    // Reserved word "if"
+    TOK_THEN,  // Reserved word "then"
+    TOK_ELSE,  // Reserved word "else"
+    TOK_FI,    // Reserved word "fi"
+    TOK_DO,    // Reserved word "do"
+    TOK_DONE,  // Reserved word "done"
+    TOK_CASE,  // Reserved word "case"
+    TOK_ESAC,  // Reserved word "esac"
+    TOK_WHILE, // Reserved word "while"
+    TOK_UNTIL, // Reserved word "until"
+    TOK_FOR,   // Reserved word "for"
+    TOK_IN,    // Reserved word "in"
+    TOK_EOF,
+    TOK_UNKNOWN
+} e_tok_type;
 
-typedef struct s_tok *t_token;
+typedef enum {
+	NORMAL,
+	IN_SINGLE_QUOTES,
+	IN_DOUBLE_QUOTES
+} e_lex_state;
 
-typedef struct {
+typedef struct s_tok {
 	e_tok_type type;
 	char *raw;
-} s_tok;
+} t_tok;
 
+typedef t_tok	*(*t_tokenizer)(const char **input); 
+
+t_tok	*tokenize_normal(const char **input);
+t_tok	*tokenize_single_quotes(const char **input);
+t_tok	*tokenize_double_quotes(const char **input);
+
+typedef struct s_lex {
+	e_lex_state	state;
+	t_tokenizer	get_next_token;
+} t_lex;
+
+t_tok	*create_token(int type, const char *s)
+{
+	t_tok *token = malloc(sizeof(t_tok));
+	if (token)
+	{
+		token->e_tok_type = type;
+		token->raw = strdup(s);
+		return (token);
+	}
+	return (NULL);
+}
+
+t_lex	*create_lexer(int start_state)
+{
+	t_lex *lexer = malloc(sizeof(t_lex));
+	if (lexer)
+	{
+		lexer->state = start_state;
+		if (NORMAL == start_state)
+			lexer->get_next_token = tokenize_normal;
+		if (IN_SINGLE_QUOTES == start_state)
+			lexer->get_next_token = tokenize_single_quotes;
+		if (IN_DOUBLE_QUOTES == start_state)
+			lexer->get_next_token = tokenize_double_quotes;
+		return (lexer);
+	}
+	return (NULL);
+}
 
 is_argument(const char *s)
 {
