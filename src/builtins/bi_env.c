b623df00d7bbb20d3c52e1e08e1178d6284ffc47
Daniel Yeo <dayeo@c2r7s1.42singapore.sg>
2025-02-04 21:14:21 +0800

    env command is working now

--- src/builtins/bi_env.c
+++ src/builtins/bi_env.c
@@ -11,23 +11,26 @@
 /* ************************************************************************** */
 
 #include "bi_int.h"
-/*
-void    builtin_env(char **args)
-{
-    extern char **environ;
-    int i;
 
-    if (args[1])
-    {
-        write(STDERR_FILENO, "minishell: env: too many arguments\n", 35);
-        g_exit_code = 1;
-        return ;
-    }
-    i = 0;
-    while (environ[i])
-    {
-        printf("%s\n", environ[i]);
-        i++;
-    }
-    g_exit_code = 0;
-}*/
+int    bi_env(t_state *s, char **argv, int argc)
+{
+	extern char **environ;
+	int i;
+
+	(void)s;
+	(void)argc;
+	if (!argv || !argv[0])
+		return (1);
+	if (argv[1]) // too many arguments
+	{
+		write(STDERR_FILENO, "minishell: env: too many arguments\n", 35);
+		return (1);
+	}
+	i = 0;
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+	return (0);
+}
