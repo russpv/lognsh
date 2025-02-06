a5212aebfd165fd23a7a2e9cb39c360bd68c2dd0
Daniel Yeo <dayeo@c2r7s1.42singapore.sg>
2025-02-06 18:46:20 +0800

    added cd and unset

--- src/builtins/bi_int.h
+++ src/builtins/bi_int.h
@@ -17,9 +17,12 @@
 
 int			bi_echo(t_state *s, char **args, int argc);
 int			bi_pwd(t_state *s, char **args, int argc);
+int			bi_cd(t_state *s, char **args, int argc);
 int			bi_exit(t_state *s, char **args, int argc);
 int			bi_env(t_state *s, char **argv, int argc);
+int			bi_unset(t_state *s, char **argv, int argc);
 int			bi_exec(t_state *s, char **args, int argc);
+
 
 /* This ensures block buffer is flushed during execution
  * as a background process
