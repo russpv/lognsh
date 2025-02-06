525a855a2f5e52c2bb291de4fd30f8fb5a13699c
Daniel Yeo <dayeo@c2r7s1.42singapore.sg>
2025-02-06 19:25:19 +0800

    echo an absent variable will now show an empty line. previously it saw  as a literal string

---
+++ src/builtins/bi_echo.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bi_echo.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpeavey <rpeavey@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/28 08:56:22 by dayeo             #+#    #+#             */
+/*   Updated: 2025/02/01 01:00:28 by rpeavey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "bi_int.h"
+
+/* ECHO
+ * derived from code echo.c in Bash.
+ */
+char	*expand_variable(const char *arg)
+{
+	char	*env_value;
+	
+	if (arg[0] == '$')
+	{
+		env_value = getenv(arg + 1); // remove '$' and get value
+		if (env_value)
+			return (ft_strdup(env_value));
+		return (ft_strdup("")); // return empty string if variable is not set
+	}
+	return (ft_strdup(arg));
+}
+
+
+int	bi_echo(t_state *s, char **argv, int argc)
+{
+	char	*expanded;
+	int	i;
+
+
+	(void)s;
+	i = 1;
+	while (i <argc)
+	{
+		expanded = expand_variable(argv[i]);
+		printf("%s", expanded);
+		free(expanded);
+		
+		if (i < argc - 1)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
+//old echo
+/*int	bi_echo(t_state *s, char **argv, int argc)
+{
+	int	i;
+	int	no_newline;
+
+	(void)s;
+	i = 1;
+	no_newline = 0;
+	if (!argv)
+		return (0);
+	while (argv[i] && ft_strncmp(argv[i], "-n", -1) == 0)
+	{
+		no_newline = 1;
+		i++;
+	}
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
+		if (argv[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (!no_newline)
+		printf("\n");
+	flushbuf();
+	return (0);
+}*/
