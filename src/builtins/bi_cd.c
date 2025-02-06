2e1872e0d45b5a94eadc3e74f7d4b3ff6f4a90b2
Daniel Yeo <dayeo@c2r7s1.42singapore.sg>
2025-02-06 19:00:09 +0800

    cd for relative and absolute path

--- src/builtins/bi_cd.c
+++ src/builtins/bi_cd.c
@@ -12,43 +12,51 @@
 
 #include "bi_int.h"
 
-/*
-void    builtin_cd(char **args)
+int	bi_cd(t_state *s, char **argv, int argc)
 {
-    char    *target_dir;
+	char    *target_dir;
+	char	*old_pwd;
+	char	*new_pwd;
 
-    // if too many arguments > error message; g_exit = 1
-    if (args[2])
-    {
-        write(STDERR_FILENO, "minishell: cd: too many arguments\n", 36);
-        g_exit_code = 1;
-        return ;
-    }
-    // if no args > go to HOME
-    if (!args[1])
-    {
-        target_dir = getenv("HOME");
-        if (!target_dir)
-        {
-            write(STDERR_FILENO, "minishell: cd: HOME not set\n", 28);
-            g_exit_code = 1;
-            return ;
-        }
-    }
-    // assigns directory path to target_dir
-    else
-        target_dir = args[1];
-    // if directory does not exist > error msg
-    if (chdir(target_dir) == -1)
-    {
-        write(STDERR_FILENO, "minishell: cd: ", 15);
-        write(STDERR_FILENO, target_dir, ft_strlen(target_dir));
-        write(STDERR_FILENO, ": ", 2);
-        write(STDERR_FILENO, strerror(errno), ft_strlen(strerror(errno)));
-        write(STDERR_FILENO, "\n", 1);
-        g_exit_code = 1;
-        return ;
-    }
-    // successful change dir > update prompt
-    g_exit_code = 0;
-}*/
+	(void)argc;
+	(void)s;
+	// if too many arguments error message; 
+	if (argv[2])
+	{
+		write(STDERR_FILENO, "minishell: cd: too many \
+			arguments\n", 36);
+        	return (1);
+    	}
+	// if no args > do nothing
+	if (!argv[1])
+	    	return (0);
+	// Assign directory path to target_dir
+	target_dir = argv[1];
+	// save old pwd before changing
+	old_pwd = getenv("PWD");
+    	// if directory does not exist > error msg
+	if (chdir(target_dir) == -1)
+	{
+	 	write(STDERR_FILENO, "minishell: cd: ", 15);
+	 	write(STDERR_FILENO, target_dir, ft_strlen(target_dir));
+	 	write(STDERR_FILENO, ": ", 2);
+	 	write(STDERR_FILENO, strerror(errno), ft_strlen(strerror(errno)));
+	 	write(STDERR_FILENO, "\n", 1);
+		return (1);
+	}
+    	// new working directory with dynamic allocation
+    	new_pwd = getcwd(NULL, 0);
+    	if (!new_pwd)
+    	{
+    		write(STDERR_FILENO, "minish: cd: error retrieving current directory\n", 48);
+    		return (1);
+    	}
+    	
+    	if (old_pwd)
+    		setenv("OLDPWD", old_pwd, 1);
+    	setenv("PWD", new_pwd, 1);
+    	free(new_pwd); // must free getcwd(NULL, 0)
+    	
+    	return (0);
+    	
+ }
