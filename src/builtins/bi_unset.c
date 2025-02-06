da63eb7f2709ff5ec7dadb89e46d51fcb20b9ab1
Daniel Yeo <dayeo@c2r7s1.42singapore.sg>
2025-02-06 19:16:40 +0800

    unset works

--- src/builtins/bi_unset.c
+++ src/builtins/bi_unset.c
@@ -11,3 +11,41 @@
 /* ************************************************************************** */
 
 #include "bi_int.h"
+
+extern	char	**environ;
+
+int	bi_unset(t_state *s, char **argv, int argc)
+{
+	
+	int	i;
+	int	j;
+	
+	
+	(void)s;
+	if (argc < 2)
+		return (0);
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (environ[j])
+		{
+			if (ft_strncmp(environ[j], argv[i], ft_strlen(argv[i])) == 0 \
+				&& environ[j][ft_strlen(argv[i])] == '=')
+			{
+				while (environ[j])
+				{
+					environ[j] = environ[j + 1];
+					j++;
+				}
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+		
+	
