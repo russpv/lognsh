67de028e0f1dd47424409f90f90162d34995db19
Daniel Yeo <dayeo@c1r3s6.42singapore.sg>
2025-02-11 12:42:44 +0800

    added ft_strndup

--- include/libft.h
+++ include/libft.h
@@ -71,6 +71,7 @@ void					ft_putnbr_fd(int n, int fd);
 
 size_t					ft_strlen(const char *s);
 char					*ft_strdup(const char *src);
+char					*ft_strndup(const char *s, size_t size);
 size_t					ft_strlcpy(char *dest, const char *src, size_t n);
 size_t					ft_strlcat(char *dest, const char *src, size_t n);
 char					*ft_strchr(const char *str, int ch);
