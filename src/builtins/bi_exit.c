f6b997001732efb3542833e6bbcda20a6311f8e3
Daniel Yeo <dayeo@c1r3s4.42singapore.sg>
2025-02-16 21:38:32 +0800

    reworked exit to show clearly handling of different cases

--- src/builtins/bi_exit.c
+++ src/builtins/bi_exit.c
@@ -12,59 +12,17 @@
 
 #include "bi_int.h"
 
-static int  _is_numeric(const char *str)
-{
-    int     i;
-
-    i = 0;
-    if (!str || str[0] == '\0')
-        return (0);
-    if (str[0] == '-' || str[0] == '+')
-        i++;
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        i++;
-    }
-    // will not return (1) for lone '-' amd '+' inputs
-    return (i > 0);
-}
-// RKP: needs to trigger state cleanup before exiting
-// RKP: is this args or argv?
 int	bi_exit(t_state *s, char **args, int argc)
 {
-    int     exit_code;
-    
-    (void)argc;
-    write(STDOUT_FILENO, "exit\n", 5);
-
-    // if no args: exit with last exit code
-    if (!args[1])
-    {
-        exit_code = *(get_status(s));
-        destroy_state(s);
-        exit(exit_code);
-    }
-    // if first arg is not numeric: exit with 255; shell still exits  
-    if (!_is_numeric(args[1]))
-    {   
-        destroy_state(s);
-        write(STDERR_FILENO, "minishell:exit: numeric argument required\n", 43);
-        exit (255);
-    }
-    // if too many args: update global; do not exit shell; g_exit_code updated
-    // only when shell is not exited.  
-    if (args[2])
-    {
-        destroy_state(s);
-        write(STDERR_FILENO, "minishell:exit: too many arguments\n", 36);
-        return (1); // RKP: returns exit code to caller
-    }
-    // if valid ard: change ascii to int and mod 256; if code is (-) > add 256
-    exit_code = ft_atoi(args[1]) % 256;
-    if (exit_code < 0)
-        exit_code = exit_code + 256;
-    destroy_state(s);
-    exit(exit_code);
+	(void)argc;
+	write(STDOUT_FILENO, "exit\n", 5);
+	if (!args[1])
+		handle_no_arg(s);
+	else if (!is_numeric(args[1]))
+		handle_invalid_arg(s);
+	else if (args[2])
+		handle_too_many_args(s);
+	else
+		handle_valid_arg(s, args[1]);
+	return (0);
 }
