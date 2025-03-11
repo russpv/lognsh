04fb328700e8af8a10041c2ec6dc2ea144ffc21b
Daniel Yeo <dayeo@c2r7s1.42singapore.sg>
2025-03-11 21:27:53 +0800

    reconfigured sigint handler for exec_fork_execve so that multiple prompts are not displayed

--- src/execute/exec_simple.c
+++ src/execute/exec_simple.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_simple.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dayeo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 20:28:07 by dayeo             #+#    #+#             */
+/*   Updated: 2025/03/11 20:28:12 by dayeo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "execute_int.h"
 
 #define EMSG_ECMD_NULLARGV "_do_child_ops: ERR null argv parameters\n"
@@ -44,11 +56,11 @@ static int	_do_child_ops(t_state *s)
 /* Executes redirects, if any, and calls built-in */
 int	exec_bi_call(t_state *s, t_builtin_fn bi)
 {
-	const t_cmd	*c = (const t_cmd *)get_cmd(s);
-	const t_ast_node *a = (const t_ast_node *)c_get_node((t_cmd *)c);
-	const char	**argv = (const char **)c_get_argv((t_cmd *)c);
-	const int	argvc = (const int)c_get_argvc((t_cmd *)c);
-	int		exit_code;
+	int					exit_code;
+	const t_cmd			*c = (const t_cmd *)get_cmd(s);
+	const char			**argv = (const char **)c_get_argv((t_cmd *)c);
+	const int			argvc = (const int)c_get_argvc((t_cmd *)c);
+	const t_ast_node	*a = (const t_ast_node *)c_get_node((t_cmd *)c);
 
 	debug_print(DMSG_EC_GOT, __FUNCTION__, argv[0], a);
 	if (!argv || !bi || !c || !a)
@@ -69,6 +81,7 @@ int	exec_bi_call(t_state *s, t_builtin_fn bi)
  * Returns early in case SIGINT received.
  * If anything errors in the child, exits with 127.
  */
+/* //previous exec_fork_execve
 int	exec_fork_execve(t_state *s)
 {
 	pid_t	p;
@@ -93,6 +106,72 @@ int	exec_fork_execve(t_state *s)
 		return (ERR_FORK);
 	}
 	waitchild(&exit_code, 1);
+	set_exit_status(s, exit_code);
+	s_free_cmd(s);
+	return (exit_code);
+}*/
+
+static void	ignore_or_restore_sigint(struct sigaction *sa_restore, int ignore)
+{
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	if (ignore)
+	{
+		sa.sa_handler = SIG_IGN;
+		sa.sa_flags = 0;
+		sigaction(SIGINT, &sa, sa_restore);
+	}
+	else
+	{
+		sa.sa_handler = sa_restore->sa_handler;
+		sa.sa_flags = sa_restore->sa_flags;
+		sigaction(SIGINT, &sa, NULL);
+	}
+}
+
+static void	exec_child_process(t_state *s)
+{
+	reset_signal_handlers();
+	if (_do_child_ops(s) != 0)
+		exit(ERR_CHILD_FAILED);
+}
+
+static void	reset_prompt(void)
+{
+	if (g_last_signal == SIGINT)
+	{
+		g_last_signal = 0;
+		write(STDOUT_FILENO, "\n", 1);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
+int	exec_fork_execve(t_state *s)
+{
+	struct sigaction	sa_restore;
+	pid_t				p;
+	int					exit_code;
+
+	if (g_last_signal == SIGINT)
+	{
+		g_last_signal = 0;
+		return (SIGINT_BEFORE_FORK);
+	}
+	ignore_or_restore_sigint(&sa_restore, 1);
+	p = fork();
+	if (p == 0)
+		exec_child_process(s);
+	else if (p < 0)
+	{
+		perror(EMSG_FORK);
+		ignore_or_restore_sigint(&sa_restore, 0);
+		return (ERR_FORK);
+	}
+	waitchild(&exit_code, 1);
+	ignore_or_restore_sigint(&sa_restore, 0);
+	reset_prompt();
 	set_exit_status(s, exit_code);
 	s_free_cmd(s);
 	return (exit_code);
