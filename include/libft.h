95cad19a58e1c7a29a798379b5bde7b6b2eda00e
Daniel Yeo <dayeo@c2r7s1.42singapore.sg>
2025-03-11 17:15:10 +0800

    added ft_strjoin_free.c

--- include/libft.h
+++ include/libft.h
@@ -105,6 +105,7 @@ char					*ft_strmapi(char const *s, char (*f)(unsigned int,
 char					*ft_substr(char const *s, unsigned int start,
 							size_t len);
 char					*ft_strjoin(char const *s1, char const *s2);
+char					*ft_strjoin_free(char *s1, const char *s2);
 char					*ft_strtrim(char const *s1, char const *set);
 char					**ft_split(char const *s, char c);
 
