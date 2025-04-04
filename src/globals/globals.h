82d8b126bcc9c5adb8572e71ffc8425e921027b4
Daniel Yeo <dayeo@c1r1s5.42singapore.sg>
2025-04-04 15:24:04 +0800

    globals normed

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   globals.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dayeo <dayeo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 14:44:36 by dayeo             #+#    #+#             */
+/*   Updated: 2025/04/04 14:44:49 by dayeo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GLOBALS_H
 # define GLOBALS_H
 
 # include <signal.h>
 
 # define SHELL_NAME "minish"
-# define DFL_PROMPT GREEN SHELL_NAME "> " RESET
+//# define DFL_PROMPT GREEN SHELL_NAME "> " RESET
+# define DFL_PROMPT "\001\033[32m\002minish> \001\033[0m\002"
 # define HDOC_PROMPT "> "
 # define INPUT_BUF_SZ 1024
 # define IFS " \t\n"
